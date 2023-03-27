3ab74681d2fdbf0b01f2a75ec0956029bb5513f2
Ji Myoung Ha <NoPlayer40600@gmail.com>
2023-03-27 14:37:59 +0900

    스프링 부트 프로젝트 생성
    Created Spring Boot Project

---
+++ src/main/java/kr/or/argos/homeless/HomelessApplication.java
@@ -0,0 +1,13 @@
+package kr.or.argos.homeless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomelessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomelessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/or/argos/homeless/HomelessApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.argos.homeless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomelessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
