7da500837160a57cf0ae57c627fb5b8b5fc18ece
wjdtjdgns <person3275@gmail.com>
2023-05-21 00:49:02 +0900

    feat: Background image 구현

--- src/ui/home/home_notice/HomeNotice.tsx
+++ src/ui/home/home_notice/HomeNotice.tsx
@@ -1,17 +1,35 @@
 import React from "react";
 import DivideContainer from "../../widget/Divider";
+import { ColorConstant } from "../../../GlobalConstant";
+import FlexContainer from "../../widget/FlexContainer";
 
-type HomeNoticeProps = {
-    screen_height: String
+const DUMMY_NOTICE = {
+  imageUrl: require("../../../assets/images/noticeImage.png"),
+
 };
 
-const HomeNotice = ({
-    screen_height,
-}: HomeNoticeProps) => {
-    return (
-        <DivideContainer {...{ minHeight: screen_height }}>
-        </DivideContainer>
-    );
-}
+type HomeNoticeProps = {
+  screen_height: String;
+};
+
+const HomeNotice = ({ screen_height }: HomeNoticeProps) => {
+  return (
+    <DivideContainer
+      direction="column"
+      {...{ minHeight: screen_height, backgroundColor: ColorConstant.BLACK }}
+    >
+      <FlexContainer
+        height="75vh"
+        {...{
+          backgroundImage: `url(${DUMMY_NOTICE.imageUrl})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center top",
+        }}
+      >
+      </FlexContainer>
+    </DivideContainer>
+  );
+};
 
 export default HomeNotice;
