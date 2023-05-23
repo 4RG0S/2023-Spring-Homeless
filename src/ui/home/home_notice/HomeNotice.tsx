8e6e51421744548e5123eb3e6bc1a716889f42fa
wjdtjdgns <person3275@gmail.com>
2023-05-23 16:12:37 +0900

    feat: 공지 바로가기 링크 추가

--- src/ui/home/home_notice/HomeNotice.tsx
+++ src/ui/home/home_notice/HomeNotice.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import DivideContainer from "../../widget/Divider";
 import { ColorConstant } from "../../../GlobalConstant";
 import FlexContainer from "../../widget/FlexContainer";
+import { ResponsiveText } from "../../widget/TextWidgets";
 
 const DUMMY_NOTICE = {
   imageUrl: require("../../../assets/images/noticeImage.png"),
-
+  url: "/",
 };
 
 type HomeNoticeProps = {
@@ -27,6 +29,25 @@ const HomeNotice = ({ screen_height }: HomeNoticeProps) => {
           backgroundPosition: "center top",
         }}
       >
+        <FlexContainer width="100%" alignItems="flex-start" justifyContent="space-between" {...{margin: "3% 3%"}}>
+          <ResponsiveText {...{color: "white", margin: "0"}}>
+            Our best contribution
+            <br />
+            to the world
+          </ResponsiveText>
+          <Link
+            to={DUMMY_NOTICE.url}
+            style={{
+              color: "white",
+              fontWeight: 700,
+              textDecorationColor: "white",
+              textDecorationThickness: "0.3px",
+              WebkitTextStroke: "0.3px black",
+            }}
+          >
+            Learn more &gt;
+          </Link>
+        </FlexContainer>
       </FlexContainer>
     </DivideContainer>
   );
