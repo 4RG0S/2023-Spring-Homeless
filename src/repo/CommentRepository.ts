e1c7f348ccd00444153dfe3e68836d4789a77071
purplepig4657 <80941516+purplepig4657@users.noreply.github.com>
2023-05-25 09:02:27 +0900

    fix: add getComments method at CommentRepository

--- src/repo/CommentRepository.ts
+++ src/repo/CommentRepository.ts
@@ -5,6 +5,7 @@ export default interface CommentRepository {
     createComment(comment: CommentRequest): number;
     getComment(commentedPostBoard: Board, commentId: number): Comment;
     getCommentsByUser(userId: string): Array<Comment>;
+    getCommentsByPost(postedBoard: Board, postId: number): Array<Comment>;
     // updateComment(commentedPostBoard: Board, commentId: number, updatedComment: CommentRequest): boolean;
     updateComment(updatedComment: Comment): boolean;
     deleteComment(commentedPostBoard: Board, commentId: number): boolean;
