8a28a28953e32a87a62e1e08e9871688530a6ade
purplepig4657 <80941516+purplepig4657@users.noreply.github.com>
2023-06-25 20:05:03 +0900

    feat: add post card with thumbnail

--- src/ui/posts/post_card/PostCard.tsx
+++ src/ui/posts/post_card/PostCard.tsx
@@ -10,37 +10,66 @@ type PostCardProps = {
     post: Post
 }
 
-
 const PostCard = ({post}: PostCardProps): JSX.Element => {
-    const createdAt = `${post.createdAt.getFullYear()}.${post.createdAt.getMonth()}.${post.createdAt.getDate()}.`
-    const imgURL = post.thumbnailURL ? post.thumbnailURL : logo;
-    
     return (
         <FlexContainer flexDirection='column' 
-            {...{background: ColorConstant.BLACK, padding: '0 20px', borderRadius: '20px'}}>
-            <FlexContainer justifyContent='space-between' alignItems="center">
-                <ResponsiveText fontSize='Small' fontWeight={200} color={ColorConstant.WHITE}>
-                    {post.user.name} | {createdAt}
-                </ResponsiveText>
-                <UserIconSmall imgURL={imgURL} />
-            </FlexContainer>
-            <ResponsiveText fontSize="Small" fontWeight={400} color={ColorConstant.WHITE} wordWarp="break-word">
-                {post.title}
-            </ResponsiveText>
-            <ResponsiveText fontSize="Small" fontWeight={200} color={ColorConstant.DEEP_GREY} {...{margin: '0'}}>
-                {post.postedBoard} Board
-            </ResponsiveText>
-            <FlexContainer flexWrap="wrap" {...{margin: '10px 0 20px 0'}}>
-                {post.tags.map(tag => (
-                    <ResponsiveText key={tag} fontSize="Small" fontWeight={200} color={ColorConstant.LIGHT_BLUE} 
-                        {...{margin: '0 10px 0 0'}}>
-                        #{tag}
-                    </ResponsiveText>
-                ))}
-            </FlexContainer>
+            {...{background: ColorConstant.BLACK, borderRadius: '20px'}}>
+            <Thumbnail post={post} />
+            <div style={{padding: '0 20px'}}>
+                <UserDateIcon post={post} />
+                <TitleBoard post={post} />
+                <Tags post={post} />
+            </div>
         </FlexContainer>
     );
 };
+
+const Thumbnail = ({post}: PostCardProps) => {
+    if (post.thumbnailURL)
+        return (<img src={post.thumbnailURL} width='100%' height='200px' 
+            style={{borderRadius: '20px', objectFit: 'cover'}} />);
+    else return <></>;
+}
+
+const UserDateIcon = ({post}: PostCardProps) => {
+    const createdAt = `${post.createdAt.getFullYear()}.${post.createdAt.getMonth()}.${post.createdAt.getDate()}.`
+    const imgURL = post.thumbnailURL ? post.thumbnailURL : logo;
+
+    return (
+        <FlexContainer justifyContent='space-between' alignItems="center">
+            <ResponsiveText fontSize='Small' fontWeight={200} color={ColorConstant.WHITE}>
+                {post.user.name} | {createdAt}
+            </ResponsiveText>
+            <UserIconSmall imgURL={imgURL} />
+        </FlexContainer>
+    );
+}
+
+const TitleBoard = ({post}: PostCardProps) => {
+    return (
+        <>
+        <ResponsiveText fontSize="Small" fontWeight={400} color={ColorConstant.WHITE} wordWarp="break-word">
+                {post.title}
+        </ResponsiveText>
+        <ResponsiveText fontSize="Small" fontWeight={200} color={ColorConstant.DEEP_GREY} {...{margin: '0'}}>
+            {post.postedBoard} Board
+        </ResponsiveText>
+        </>
+    );
+}
+
+const Tags = ({post}: PostCardProps) => {
+    return (
+        <FlexContainer flexWrap="wrap" {...{margin: '10px 0 20px 0'}}>
+            {post.tags.map(tag => (
+                <ResponsiveText key={tag} fontSize="Small" fontWeight={200} color={ColorConstant.LIGHT_BLUE} 
+                    {...{margin: '0 10px 0 0'}}>
+                    #{tag}
+                </ResponsiveText>
+            ))}
+        </FlexContainer>
+    );
+}
 
 const UserIconSmall = ({imgURL}: {imgURL: string}) => (
     <img src={imgURL} width='35rem' height='35rem' style={{borderRadius: '50%', objectFit: 'cover'}} />
